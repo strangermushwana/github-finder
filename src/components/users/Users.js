015bf0ef8007afc39078854b1b73a866b6e5afb7
Stranger Mushwana <strangermushwana@gmail.com>
2021-03-07 12:54:47 +0200

    Added API keys

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,16 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
+import Spinner from '../layout/Spinner'
 import UserItem from './UserItem'
+import PropTypes from 'prop-types'
 
-export class Users extends Component {
-  render() {
-    return (
-      <div style={userStyle}>
-        {this.props.users.map((user) => {
-          return <UserItem key={user.id} user={user} />
-        })}
-      </div>
-    )
-  }
+const Users = ({ users, loading }) => {
+  if (loading) return <Spinner />
+  return (
+    <div style={userStyle}>
+      {users.map((user) => {
+        return <UserItem key={user.id} user={user} />
+      })}
+    </div>
+  )
+}
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
 }
 
 const userStyle = {
