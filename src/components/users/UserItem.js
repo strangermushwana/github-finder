af4ca2929a0f1993a6fee20a32815c102fd7d58f
Stranger Mushwana <strangermushwana@gmail.com>
2021-03-07 10:42:01 +0200

    Added UserItem Component

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+export class UserItem extends Component {
+  state = {
+    id: 'id',
+    login: 'mojombo',
+    avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
+    html_url: 'https://github.com/mojombo',
+  }
+
+  render() {
+    const { login, avatar_url, html_url } = this.state
+    return (
+      <div className='card text-center'>
+        <img
+          src={avatar_url}
+          className='round-img'
+          alt='Avatar'
+          style={{ width: '60px' }}
+        />
+        <h3>{login}</h3>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+          More
+        </a>
+      </div>
+    )
+  }
+}
+
+export default UserItem
