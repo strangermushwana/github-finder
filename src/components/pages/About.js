c11a95bf46486f39c79e2b8adf88b888dedec944
Stranger Mushwana <strangermushwana@gmail.com>
2021-03-07 14:00:54 +0200

    Added React router and about page

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p>App for searching Github users</p>
+      <p>Verson: 1.0.0</p>
+    </div>
+  )
+}
+
+export default About
